ea560f6351b0090d892dd639e4f5fd7d1490ed05
nicolaihald <haldcph@gmail.com>
2016-06-10 16:13:31 +0200

    logstash: Added LOGSTASH_CFG_FILE environment variable for adding support for downloading the logstash configuration file during container startup.

--- logstash/start.sh
+++ logstash/start.sh
@@ -12,4 +12,15 @@ if [ "$1" = 'logstash' ]; then
   set -- gosu logstash "$@"
 fi
 
+
+LOGSTASH_CFG_FILE="/config/logstash.conf"
+# Download the config file if given a URL
+if [ ! -z ${LOGSTASH_CFG_URL} ]; then
+  wget -O ${LOGSTASH_CFG_FILE} ${LOGSTASH_CFG_URL}
+  if [ $? -ne 0 ]; then
+    echo "[LOGSTASH] Unable to download file ${LOGSTASH_CFG_URL}."
+    exit 1
+  fi
+fi
+
 exec "$@"
